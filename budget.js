5a6e3740080b334dd6405df8146412c88e4d82c0
tranthucdoan <kathleentran@Kathleens-MacBook-Pro-2.local>
2014-07-12 16:54:17 -0500

    Saved user inputs in local storage

--- budget.js
+++ budget.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    var _daily = localStorage.getItem('daily'),
-        $income = document.getElementById('income'),
+    var $income = document.getElementById('income'),
         $expenses = document.getElementById('expenses'),
         $goal = document.getElementById('goal'),
-        $today = document.getElementById('today');
+        $today = document.getElementById('today'),
+        _daily = localStorage.getItem('daily'),
+        _income = localStorage.getItem('income'),
+        _expenses = localStorage.getItem('expenses'),
+        _goal = localStorage.getItem('goal');
 
+        $income.value = _income;
+        $expenses.value = _expenses;
+        $goal.value = _goal;
         $today.textContent = _daily;
 
     document.getElementById('submit').onclick = function() {
@@ -19,8 +25,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
         daily = (budget / 30).toFixed(2);
 
-        localStorage.setItem('daily', daily);
-
         $today.textContent = daily;
+
+        localStorage.setItem('daily', daily);
+        localStorage.setItem('income', income);
+        localStorage.setItem('expenses', expenses);
+        localStorage.setItem('goal', goal);
     };
 });
